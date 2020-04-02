17dfdfa0eec0e34c9a0f550f988b6d19c1ac18aa
Eugene Yap <eugeneyapwc93@gmail.com>
2020-04-01 21:59:48 -0400

    Implement AuthorController and thymeleaf templates for authors

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repository.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthor(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors/authorList";
+    }
+}
